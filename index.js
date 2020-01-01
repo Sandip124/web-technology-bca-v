a0e02186ea86b36be733cb76c66c0ce168dd4465
sandip124 <chaudharysandip124@gmail.com>
2020-01-01 08:24:01 +0545

    web technology works

--- index.js
+++ index.js
@@ -1,34 +1,91 @@
 var fName = document.getElementById("firstName");
 var lName = document.getElementById("lastName");
 var age = document.getElementById("age");
+
+var eFName = document.getElementById("error-firstName");
+var eLName = document.getElementById("error-lastName");
+var eAge = document.getElementById("error-age");
+
 var submitBtn = document.getElementById("submitBtn");
+var dynamicDataDiv = document.getElementById("dynamicData");
 
     submitBtn.addEventListener("click",validate);
 
     function validate()
     {
+        defaultErrorValidation();
+
         console.log(fName.value);
         if(fName.value == "")
         {
-            alert("firstName is Empty.");
+            showErrorForName("firstName is Empty.");
         }else if(fName.value.length < 3)
         {
-            alert("At least 3 character needed.");
+            showErrorForName("At least 3 character needed.");
         }   
 
         if(lName.value == "")
         {
-            alert("Last Name is required");
+            // alert("Last Name is required");
+            showErrorForLastName("Last Name is required");
         } else if(lName.value.length < 3)
         {
-            alert("At least 3 character needed.");
+            showErrorForLastName("At least 3 character needed.");
         }
 
-        if(age.value  <= 0)
+        if(isNaN(age.value))
         {
-            alert("age cannot be zero and less than zero.");
-        }else if(age.value >= 100)
-        {
-            alert("Max age is 100.");
+            showErrorForAge("Ony numbers are allowed.");
+        }else{
+            console.log(age.value);
         }
+        if(age.value  < 1)
+        {
+            showErrorForAge("age cannot be zero and less than zero.");
+        }else if(age.value > 100)
+        {
+            showErrorForAge("Max age is 100.");
+        }
+
+        // console.log(dynamicDataDiv);
+        
+        var output = `<tr><td>`+fName.value+`</td>
+        <td>`+lName.value+`</td>
+        <td>`+age.value+`</td></tr>`;
+        
+        dynamicDataDiv.innerHTML +=output;
+
+        function showErrorForName(message)
+        {
+            eFName.style.display ="block";
+            eFName.style.color ="red";
+            eFName.innerHTML=message;
+        }
+
+        
+       function showErrorForLastName(message)
+       {
+        eLName.style.display ="block";
+        eLName.style.color ="red";
+        eLName.innerHTML=message;
+       }
+        
+       function showErrorForAge(message)
+       {
+           eAge.style.display ="block";
+           eAge.style.color ="red";
+           eAge.innerHTML=message;
+       }
+
+
+        function defaultErrorValidation()
+        {
+            eFName.style.display ="none";
+            eLName.style.display ="none";
+            eAge.style.display ="none";
+        }
+
+
+
+
     }
